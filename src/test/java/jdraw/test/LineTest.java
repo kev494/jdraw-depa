859b51e8e51f9a20ddf340c65cfce41fa67a9320
Kevin Schaefer <kevin.schaefer13@gmail.com>
2018-12-18 12:58:04 +0100

    Tests with factory method

---
+++ src/test/java/jdraw/test/LineTest.java
@@ -0,0 +1,11 @@
+package jdraw.test;
+
+import jdraw.figures.Line;
+import jdraw.framework.Figure;
+
+public class LineTest extends AbstractFigureTest{
+	@Override
+	protected Figure createFigure(int x, int y, int w, int h) {
+		return new Line(x, y, w, h);
+	}
+}
